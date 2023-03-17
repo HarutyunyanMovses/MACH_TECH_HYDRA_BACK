cc2ced7e51619bc97737dd002427df017c20a22d
HarutyunyanMovses <107640630+HarutyunyanMovses@users.noreply.github.com>
2023-03-18 00:05:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports.SECRET = {
     MONGO_URL:"mongodb+srv://admin:admin@cluster0.xgwn7ch.mongodb.net/Cluster0",
     PORT : 3030,
     URL_LOCAL_SERVER: "https://hydra-machtech-back.herokuapp.com",
-    URL:"http://localhost:3000",
+    URL:"https://hydra-machtech.herokuapp.com",
 }
 
